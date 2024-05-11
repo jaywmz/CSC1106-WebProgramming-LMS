03c07655a9919bc8a681d7700a6504733ab15fe8
Leo Oh <leooh29@gmail.com>
2024-05-11 12:08:41 +0800

    Added example GET in HomeController

--- Web Programming/demo/src/main/java/demo/demo/HomeController.java
+++ Web Programming/demo/src/main/java/demo/demo/HomeController.java
@@ -1,14 +1,36 @@
 package demo.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    // One syntax to implement a
+    // GET method
     @GetMapping("/")
-    public String home() {
-        return "Hello, World!";
+    public String home()
+    {
+        String str
+            = "<html><body><font color=\"Blue\">"
+              + "<h1>WELCOME</h1>"
+              + "</font></body></html>";
+        return str;
     }
-
+  
+    // Another syntax to implement a
+    // GET method
+    @RequestMapping(
+        method = { RequestMethod.GET },
+        value = { "/home2" })
+    public String info()
+    {
+        String str2
+            = "<html><body><font color=\"Red\">"
+              + "<h2>WELCOME AGAIN"
+              + "</h2></font></body></html>";
+        return str2;
+    }
 }
