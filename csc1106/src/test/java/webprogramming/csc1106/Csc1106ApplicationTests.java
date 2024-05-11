72c17228467ccc36ea61de6819324e9e011380ad
jonathanljc <151852608+jonathanljc@users.noreply.github.com>
2024-05-11 13:40:44 +0800

    update new

---
+++ csc1106/src/test/java/webprogramming/csc1106/Csc1106ApplicationTests.java
@@ -0,0 +1,13 @@
+package webprogramming.csc1106;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Csc1106ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
