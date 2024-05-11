71943576a94f53e4fcf9955b746dcd8861d13518
jonathanljc <151852608+jonathanljc@users.noreply.github.com>
2024-05-12 00:38:39 +0800

    added a login page for testing and redirect to another page after login, no database yet just normal login.

---
+++ csc1106/src/main/java/webprogramming/csc1106/Controllers/LoginController.java
@@ -0,0 +1,27 @@
+package webprogramming.csc1106.Controllers;
+
+import webprogramming.csc1106.Entities.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String showLoginPage(Model model) {
+        model.addAttribute("user", new User());
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute User user) {
+        // Handle login
+        // For now, just print the username and password
+        System.out.println("Username: " + user.getUsername());
+        System.out.println("Password: " + user.getPassword());
+        return "redirect:/forum";
+    }
+}
