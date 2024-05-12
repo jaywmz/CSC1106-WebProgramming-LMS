4f10e63c61fe468f753c0086d6926f74d4c29bb0
NineGuest <71936949+ninguest@users.noreply.github.com>
2024-05-12 21:17:36 +0800

    commit template

---
+++ demo/src/main/java/com/springcheng/demo/DemoController.java
@@ -0,0 +1,17 @@
+package com.springcheng.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class DemoController {
+    
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+    
+}
