b2e5a04a32c2927ff7f847535b34fe51d3afb92c
Jiang Weimin <lichtwm@hotmail.com>
2024-05-13 16:07:29 +0800

    test

--- csc1106/src/main/java/webprogramming/csc1106/Controllers/MarketPlaceController.java
+++ csc1106/src/main/java/webprogramming/csc1106/Controllers/MarketPlaceController.java
@@ -11,7 +11,7 @@ public class MarketPlaceController {
 
     @GetMapping("/MarketPlace")
     public String showMarketPlacePage(Model model) {
-        model.addAttribute("title", "Market Place");
+        model.addAttribute("title!", "Market Place");
         model.addAttribute("searchQuery", "");
         model.addAttribute("categories", List.of("Category 1", "Category 2", "Category 3"));
         return "MarketPlace";  // This should be the name of your HTML file without the .html extension
