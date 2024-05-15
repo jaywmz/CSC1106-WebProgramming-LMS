98960c284882535be7d24d24a230390b3c75e915
jonathanljc <151852608+jonathanljc@users.noreply.github.com>
2024-05-15 21:33:23 +0800

    added a checkout page, redirection from cart icon in the marketplace page.

--- csc1106/src/main/java/webprogramming/csc1106/Controllers/MarketPlaceController.java
+++ csc1106/src/main/java/webprogramming/csc1106/Controllers/MarketPlaceController.java
@@ -4,16 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
+
 
 @Controller
 public class MarketPlaceController {
 
-    @GetMapping("/MarketPlace")
+    @GetMapping("/Market")
     public String showMarketPlacePage(Model model) {
-        model.addAttribute("title!", "Market Place");
-        model.addAttribute("searchQuery", "");
-        model.addAttribute("categories", List.of("Category 1", "Category 2", "Category 3"));
-        return "MarketPlace";  // This should be the name of your HTML file without the .html extension
+        return "Market"; 
+    }
+    @GetMapping("/checkout")
+    public String redirectToCheckout() {
+        return "checkout";  // when click the cart icon in the market page, it will redirect to the checkout page
     }
 }
