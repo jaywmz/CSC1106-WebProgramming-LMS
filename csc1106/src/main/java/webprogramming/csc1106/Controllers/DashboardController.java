956f2df748df72521bfd5154e56df8b6a0dc4d5e
Liangzhi61 <162430671+Liangzhi61@users.noreply.github.com>
2024-05-19 18:40:20 +0800

    Add files via upload

---
+++ csc1106/src/main/java/webprogramming/csc1106/Controllers/DashboardController.java
@@ -0,0 +1,39 @@
+package webprogramming.csc1106.Controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
+
+import webprogramming.csc1106.Entities.User;
+import webprogramming.csc1106.Repositories.UserRepository;
+import webprogramming.csc1106.Securities.Encoding;
+
+
+
+@Controller
+public class DashboardController {
+    private final UserRepository userRepository;
+    public DashboardController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/board/{email}")
+     public ModelAndView userProfile(@PathVariable String email) {
+        ModelAndView modelAndView = new ModelAndView("pages-dashboard");
+        // Decode the email address
+        String decodedEmail = Encoding.decode(email);
+        User user = userRepository.findByUserEmail(decodedEmail);
+
+        if (user != null) {
+            // Pass the user object to the view
+
+            modelAndView.addObject("user", user);
+            modelAndView.addObject("encoded", email);
+        } else {
+            // Handle user not found
+        }
+
+        return modelAndView;
+    }
+    
+}
