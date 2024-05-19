c674e38de46ccdca7024991c286193ebaa2649b0
Liangzhi61 <162430671+Liangzhi61@users.noreply.github.com>
2024-05-19 18:35:20 +0800

    Update LoginController.java

--- csc1106/src/main/java/webprogramming/csc1106/Controllers/LoginController.java
+++ csc1106/src/main/java/webprogramming/csc1106/Controllers/LoginController.java
@@ -1,19 +1,16 @@
 package webprogramming.csc1106.Controllers;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
 import webprogramming.csc1106.Entities.User;
 import webprogramming.csc1106.Repositories.UserRepository;
+import webprogramming.csc1106.Securities.Encoding;
 
 @Controller
 public class LoginController {
-
-    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 
     private final UserRepository userRepository;
 
@@ -23,27 +20,24 @@ public class LoginController {
 
     @GetMapping("/login")
     public String loginForm() {
-        logger.debug("GET request received for login form");
         return "pages-login"; // Return the login form
     }
 
     @PostMapping("/login")
-    public ModelAndView login(@RequestParam String username, @RequestParam String password) {
-        logger.debug("POST request received for login");
-        logger.debug("Username: {}", username);
-        logger.debug("Password: {}", password);
-
+    public ModelAndView login(@RequestParam String email, @RequestParam String password) {
         ModelAndView modelAndView = new ModelAndView();
 
-        // Query the database for the user with the given username and password
-        User user = userRepository.findByUserNameAndUserPassword(username, password);
+        // Query the database for the user with the given email and password
+        User user = userRepository.findByUserEmailAndUserPassword(email, password);
 
-        if (user == null) {
+        if (user != null) {
+            // Encode email address
+            String encodedEmail = Encoding.encode(email);
+            // Redirect to profile page with email as a parameter
+            modelAndView.setViewName("redirect:/board/" + encodedEmail);
+        } else {
             // No user found, redirect to login page with an error message
             modelAndView.setViewName("redirect:/login?error=true");
-        } else {
-            // User found, redirect to dashboard or success page
-            modelAndView.setViewName("redirect:/dashboard");
         }
 
         return modelAndView;
