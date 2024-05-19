03d78351dd7fc3f2c1342f690b0792321888b957
Liangzhi61 <162430671+Liangzhi61@users.noreply.github.com>
2024-05-19 18:50:00 +0800

    Update UserRepository.java

--- csc1106/src/main/java/webprogramming/csc1106/Repositories/UserRepository.java
+++ csc1106/src/main/java/webprogramming/csc1106/Repositories/UserRepository.java
@@ -1,8 +1,14 @@
 package webprogramming.csc1106.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import webprogramming.csc1106.Entities.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByUserNameAndUserPassword(String userName, String userPassword);
+    User findByUserEmailAndUserPassword(String userEmail, String userPassword); // For Login Purposes
+    User findByUserName(String userName); //For getting username
+    User findByRoleID(Integer roleID);
+    User findByUserEmail(String userEmail); // For getting user by email
+    //User findByHashedUserName(String hashedUserName); // New method to find user by hashed username
+
 }
