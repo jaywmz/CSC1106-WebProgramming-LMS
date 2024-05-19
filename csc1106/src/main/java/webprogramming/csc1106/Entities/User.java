f1c519773a9e9ae73b1c7f646e70802b9735e7ab
Liangzhi61 <162430671+Liangzhi61@users.noreply.github.com>
2024-05-19 19:19:18 +0800

    Update User.java

--- csc1106/src/main/java/webprogramming/csc1106/Entities/User.java
+++ csc1106/src/main/java/webprogramming/csc1106/Entities/User.java
@@ -1,14 +1,15 @@
 package webprogramming.csc1106.Entities;
 
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.sql.Time;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import java.sql.Date;
-import java.sql.Time;
-import java.math.BigDecimal;
 
 @Entity
 @Table(name = "users")
@@ -16,6 +17,7 @@ public class User {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Integer userID;
 
     @Column(name = "role_id")
