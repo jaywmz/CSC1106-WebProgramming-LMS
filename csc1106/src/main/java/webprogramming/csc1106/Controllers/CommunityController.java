6e7492797736bc4a4b4f65446dd675b5505a10e3
Felix Chang <2014.felixchang@gmail.com>
2024-06-03 20:57:40 +0800

    Community: Search posts features

--- csc1106/src/main/java/webprogramming/csc1106/Controllers/CommunityController.java
+++ csc1106/src/main/java/webprogramming/csc1106/Controllers/CommunityController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import webprogramming.csc1106.Entities.*;
-import webprogramming.csc1106.Repositories.CategoryRepo;
+import webprogramming.csc1106.Repositories.PostRepo;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,14 +16,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CommunityController {
+
+    @Autowired
+    private PostRepo postRepo;
     
     @GetMapping("/community")
     public String getCommunityHome() {
         return "community-home";
     }
     
-    // @GetMapping("/community/{category_name}")
-    // public String getCategory(@PathVariable String category_name) {
-    //     return "redirect:/" + category_name;
-    // }
+    @GetMapping("/community/search")
+    public String getSearchResults(@RequestParam("key") String key, @RequestParam(defaultValue = "1") int page, Model model) {
+        Page<Post> queriedPosts = postRepo.findAllByTitleContainingOrContentContainingOrderByTimestampDesc(key, key, PageRequest.of(page - 1, 10));
+        
+        model.addAttribute("posts", queriedPosts.getContent());
+        model.addAttribute("currentPage", page);
+        model.addAttribute("search_term", key);
+        model.addAttribute("totalPage", queriedPosts.getTotalPages());
+        return "community-search";
+    }
 }
--- csc1106/src/main/java/webprogramming/csc1106/Repositories/PostRepo.java
+++ csc1106/src/main/java/webprogramming/csc1106/Repositories/PostRepo.java
@@ -1,8 +1,10 @@
 package webprogramming.csc1106.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 import webprogramming.csc1106.Entities.Post;
 
 public interface PostRepo extends JpaRepository<Post, Integer>{
-
-}
+    Page<Post> findAllByTitleContainingOrContentContainingOrderByTimestampDesc(String title, String content, Pageable pageable);
+}   
