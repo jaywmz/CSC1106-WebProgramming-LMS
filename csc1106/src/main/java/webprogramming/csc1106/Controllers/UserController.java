7518fa0d1bad9f48fae5dcae033b9f4e901573c1
NineGuest <71936949+ninguest@users.noreply.github.com>
2024-06-05 00:07:39 +0800

    Edit Something no that important

--- csc1106/src/main/java/webprogramming/csc1106/Controllers/UserController.java
+++ csc1106/src/main/java/webprogramming/csc1106/Controllers/UserController.java
@@ -7,30 +7,58 @@ import java.sql.Time;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import webprogramming.csc1106.Entities.User;
 import webprogramming.csc1106.Repositories.UserRepository;
+import webprogramming.csc1106.Securities.Encoding;
 
 @Controller
-public class RegisterController {
-
-    private static final Logger logger = LoggerFactory.getLogger(RegisterController.class);
+public class UserController {
 
     private final UserRepository userRepository;
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
-    public RegisterController(UserRepository userRepository) {
+    public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    @GetMapping("/login")
+    public String loginForm() {
+        return "pages-login"; // Return the login form
     }
 
     @GetMapping("/register")
     public String registerForm() {
         logger.debug("GET request received for registration form");
         return "pages-register"; // Return the registration form
+    }
+
+    @PostMapping("/login")
+    public ModelAndView login(@RequestParam String email, @RequestParam String password) {
+        ModelAndView modelAndView = new ModelAndView();
+
+        // Query the database for the user with the given email and password
+        User user = userRepository.findByUserEmailAndUserPassword(email, password);
+
+        if (user != null) {
+            // Encode email address
+            String encodedEmail = Encoding.encode(email);
+            // Redirect to profile page with email as a parameter
+            modelAndView.setViewName("redirect:/board/" + encodedEmail);
+        } else {
+            // No user found, redirect to login page with an error message
+            modelAndView.setViewName("redirect:/login?error=true");
+        }
+
+        return modelAndView;
     }
 
     @PostMapping("/register")
@@ -64,4 +92,5 @@ public class RegisterController {
         userRepository.save(user);
         logger.debug("User data saved to the database");
     }
+
 }
