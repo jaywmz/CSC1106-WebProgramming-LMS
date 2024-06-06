9696f81af414cd8798a090633c3ccbb8cf5b15b3
Felix Chang <2014.felixchang@gmail.com>
2024-06-06 20:24:43 +0800

    feat(Community): students section post retrieval

--- csc1106/src/main/java/webprogramming/csc1106/Controllers/CommunityCategoryController.java
+++ csc1106/src/main/java/webprogramming/csc1106/Controllers/CommunityCategoryController.java
@@ -11,6 +11,8 @@ import webprogramming.csc1106.Entities.*;
 import webprogramming.csc1106.Repositories.CategoryRepo;
 
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class CommunityCategoryController {
@@ -18,17 +20,19 @@ public class CommunityCategoryController {
     @Autowired
     private CategoryRepo categoryRepo;
 
-    // @GetMapping("/community/{category_name}")
-    // public String getCategoryPosts(@PathVariable String category_name, Model model) {
-    //     category_name = category_name.substring(0, 1).toUpperCase() + category_name.substring(1); // capitalise first letter of category name
-    //     model.addAttribute("category_name", category_name); // add category name to template model
+    @GetMapping("/community/students/{category_id}")
+    public String getCategoryPostsStudents(@PathVariable String category_id, Model model) {
 
-    //     CommunityCategory category = categoryRepo.findByName(category_name); // retrieve category object from db by name
-    //     List<Post> posts = category.getPosts(); // get retrieved category's posts
-    //     model.addAttribute("posts", posts); // add posts to template model
+        CommunityCategory category = categoryRepo.findById(Integer.parseInt(category_id)); // retrieve category object from db by name
+        List<Post> posts = category.getPosts(); // get retrieved category's posts
+        
+        model.addAttribute("user_group", "students");
+        model.addAttribute("category_name", category.getName()); // add category name to template model
+        model.addAttribute("category_id", category_id); // used for new post later
+        model.addAttribute("posts", posts); // add posts to template model
 
-    //     return "Community/community-category";
-    // }
+        return "Community/community-category";
+    }
 
     @GetMapping("/community/announcements")
     public String getAnnouncements(Model model) {
