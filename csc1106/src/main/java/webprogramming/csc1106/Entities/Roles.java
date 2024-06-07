1189ddbdffea285073ea4a135e92e30d5cfc958c
Liangzhi61 <162430671+Liangzhi61@users.noreply.github.com>
2024-06-07 16:13:55 +0800

    Add files via upload

--- csc1106/src/main/java/webprogramming/csc1106/Entities/Roles.java
+++ csc1106/src/main/java/webprogramming/csc1106/Entities/Roles.java
@@ -1,6 +1,11 @@
 package webprogramming.csc1106.Entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "roles")
@@ -39,3 +44,4 @@ public class Roles {
         this.roleName = roleName;
     }
 }
+
