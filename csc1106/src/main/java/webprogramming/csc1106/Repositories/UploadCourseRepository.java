c69c1aedaf56478eaaed20ca7f4dc0517bcf19a5
Jiang Weimin <lichtwm@hotmail.com>
2024-06-11 17:37:53 +0800

    feat: implement course filtering and sorting

--- csc1106/src/main/java/webprogramming/csc1106/Repositories/UploadCourseRepository.java
+++ csc1106/src/main/java/webprogramming/csc1106/Repositories/UploadCourseRepository.java
@@ -2,8 +2,9 @@ package webprogramming.csc1106.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import webprogramming.csc1106.Entities.UploadCourse;
+import org.springframework.data.domain.Sort;
 import java.util.List;
 
 public interface UploadCourseRepository extends JpaRepository<UploadCourse, Long> {
-    List<UploadCourse> findByCourseCategories_CategoryGroup_Id(Long categoryId);
+    List<UploadCourse> findByCourseCategories_CategoryGroup_Id(Long categoryId, Sort sort);
 }
