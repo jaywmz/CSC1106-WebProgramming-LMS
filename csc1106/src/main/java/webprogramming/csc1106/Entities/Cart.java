f35e5f915b973b0f45b6f0edd19cf9bdfec57776
jonathanljc <151852608+jonathanljc@users.noreply.github.com>
2024-07-04 10:53:54 +0800

    comment

--- csc1106/src/main/java/webprogramming/csc1106/Entities/Cart.java
+++ csc1106/src/main/java/webprogramming/csc1106/Entities/Cart.java
@@ -10,36 +10,43 @@ public class Cart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private Long id;
+    private Long id;  // Primary key for the Cart entity, automatically generated
 
     @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<CartItem> items = new ArrayList<>();
+    private List<CartItem> items = new ArrayList<>();  // List to hold CartItem entities associated with this Cart
 
+    // Default constructor
     public Cart() {}
 
+    // Getter for the 'id' field
     public Long getId() {
         return id;
     }
 
+    // Setter for the 'id' field
     public void setId(Long id) {
         this.id = id;
     }
 
+    // Getter for the 'items' list
     public List<CartItem> getItems() {
         return items;
     }
 
+    // Setter for the 'items' list
     public void setItems(List<CartItem> items) {
         this.items = items;
     }
 
+    // Method to add a CartItem to the cart
     public void addItem(CartItem item) {
-        items.add(item);
-        item.setCart(this);
+        items.add(item);  // Add the item to the list
+        item.setCart(this);  // Set the cart reference in the item
     }
 
+    // Method to remove a CartItem from the cart
     public void removeItem(CartItem item) {
-        items.remove(item);
-        item.setCart(null);
+        items.remove(item);  // Remove the item from the list
+        item.setCart(null);  // Clear the cart reference in the item
     }
 }
