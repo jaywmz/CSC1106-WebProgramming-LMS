7ab47ef91d2bda5de3e3c2e15f2aa11b260323f3
joweenyx <angjoween@gmail.com>
2024-07-04 14:13:46 +0800

    Update PartnerPublishRepository.java

--- csc1106/src/main/java/webprogramming/csc1106/Repositories/PartnerPublishRepository.java
+++ csc1106/src/main/java/webprogramming/csc1106/Repositories/PartnerPublishRepository.java
@@ -1,7 +1,13 @@
 package webprogramming.csc1106.Repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import webprogramming.csc1106.Entities.Partner;
 import webprogramming.csc1106.Entities.PartnerPublish;
 
 public interface PartnerPublishRepository extends JpaRepository<PartnerPublish, Integer> {
+     List<PartnerPublish> findByPartnerPartnerId(Integer partnerId);
+    List<PartnerPublish> findByPartner(Partner partner);
 }
