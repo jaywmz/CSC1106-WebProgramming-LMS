593e6b7dbd0bd745ef8e519dd31d6e3804d1bc25
joweenyx <angjoween@gmail.com>
2024-07-05 15:16:14 +0800

    Update PartnerCertificateRepository.java

--- csc1106/src/main/java/webprogramming/csc1106/Repositories/PartnerCertificateRepository.java
+++ csc1106/src/main/java/webprogramming/csc1106/Repositories/PartnerCertificateRepository.java
@@ -2,6 +2,11 @@ package webprogramming.csc1106.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import webprogramming.csc1106.Entities.PartnerCertificate;
+import webprogramming.csc1106.Entities.PartnerPublish;
+
+import java.util.List;
+
 
 public interface PartnerCertificateRepository extends JpaRepository<PartnerCertificate, Integer> {
+    PartnerCertificate findByPartnerPublish(PartnerPublish partnerPublish);
 }
