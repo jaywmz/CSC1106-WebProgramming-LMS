5059a706b6b43d6a61cbb699136525c397edf0fb
Elroy <2300950@sit.singaporetech.edu.sg>
2024-07-10 05:07:10 +0800

    updated display course page. can download file too

--- csc1106/src/main/java/webprogramming/csc1106/Repositories/UploadCourseRepository.java
+++ csc1106/src/main/java/webprogramming/csc1106/Repositories/UploadCourseRepository.java
@@ -1,6 +1,9 @@
 package webprogramming.csc1106.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import webprogramming.csc1106.Entities.UploadCourse;
 import org.springframework.data.domain.Sort;
 import java.util.List;
@@ -13,4 +16,11 @@ public interface UploadCourseRepository extends JpaRepository<UploadCourse, Long
 
     // Get specific course by course id
     UploadCourse findById(int id);
+
+    @Query("SELECT c FROM UploadCourse c " +
+           "LEFT JOIN FETCH c.sections s " +
+           "LEFT JOIN FETCH c.ratings r " +
+           "LEFT JOIN FETCH c.courseCategories cc " +
+           "WHERE c.id = :id")
+    UploadCourse findByIdWithDetails(@Param("id") Long id);
 }
